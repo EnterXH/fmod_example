333b7e808ceda98afaa123a7a37d873beb850e34
gongxinhai <gongxinhai@kunpo.cc>
2019-01-28 20:03:25 +0800

    ndk-build

--- frameworks/runtime-src/proj.android/app/jni/Android.mk
+++ frameworks/runtime-src/proj.android/app/jni/Android.mk
@@ -1,20 +1,30 @@
 LOCAL_PATH := $(call my-dir)
 
+# FMOD Shared Library
+#include $(CLEAR_VARS)
+#LOCAL_MODULE            := fmod
+#LOCAL_SRC_FILES         := ../../../../fmod/android/lib/$(TARGET_ARCH_ABI)/libfmodL.so
+#LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../../../fmod/android/inc
+#include $(PREBUILT_SHARED_LIBRARY)
+# FMOD END
+
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := cocos2dlua_shared
 
-LOCAL_MODULE_FILENAME := libcclua
+LOCAL_MODULE_FILENAME := libcocos2dlua
 
 LOCAL_SRC_FILES := \
 ../../../Classes/AppDelegate.cpp \
 hellolua/main.cpp
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../Classes
+#LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../../fmod
 
 # _COCOS_HEADER_ANDROID_BEGIN
 # _COCOS_HEADER_ANDROID_END
 
+#LOCAL_SHARED_LIBRARIES := fmod
 LOCAL_STATIC_LIBRARIES := cclua_static
 
 # _COCOS_LIB_ANDROID_BEGIN
