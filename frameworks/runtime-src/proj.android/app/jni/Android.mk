dfe482b4767e9a835638a6d254cf711473efba7c
gongxinhai <gongxinhai@kunpo.cc>
2019-01-29 12:59:36 +0800

    初始化Android项目

--- frameworks/runtime-src/proj.android/app/jni/Android.mk
+++ frameworks/runtime-src/proj.android/app/jni/Android.mk
@@ -1,13 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-# FMOD Shared Library
-#include $(CLEAR_VARS)
-#LOCAL_MODULE            := fmod
-#LOCAL_SRC_FILES         := ../../../../fmod/android/lib/$(TARGET_ARCH_ABI)/libfmodL.so
-#LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../../../fmod/android/inc
-#include $(PREBUILT_SHARED_LIBRARY)
-# FMOD END
-
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := cocos2dlua_shared
@@ -19,12 +11,10 @@ LOCAL_SRC_FILES := \
 hellolua/main.cpp
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../Classes
-#LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../../fmod
 
 # _COCOS_HEADER_ANDROID_BEGIN
 # _COCOS_HEADER_ANDROID_END
 
-#LOCAL_SHARED_LIBRARIES := fmod
 LOCAL_STATIC_LIBRARIES := cclua_static
 
 # _COCOS_LIB_ANDROID_BEGIN
